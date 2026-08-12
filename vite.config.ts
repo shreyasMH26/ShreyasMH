901a95dd5739d08b993e3031f931c4db16bf23ad
MH <adolfhitlerr26@gmail.com>
2026-08-12 23:17:57 +0530

    Modify allowedHosts in Vite config
    Updated allowedHosts for server and preview configurations.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    allowedHosts: ['shreyasmh-portfolio.onrender.com'],
+    allowedHosts: [
+      'shreyasmh.onrender.com',
+      'shreyasmh-portfolio.onrender.com',
+    ],
   },
-})
+
+  preview: {
+    allowedHosts: [
+      'shreyasmh.onrender.com',
+      'shreyasmh-portfolio.onrender.com',
+    ],
+  },
+});
